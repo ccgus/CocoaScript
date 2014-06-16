a7d9fba1efee2e06ecbf64f16f0d254a799d4bb6
Sam Deane <sam@elegantchaos.com>
2014-06-16 17:02:11 +0100

    Pass through the script URL, and set the global context name. This is in order to support javascript debugging via Safari on 10.10.

--- src/framework/mocha/MochaRuntime.m
+++ src/framework/mocha/MochaRuntime.m
@@ -568,6 +568,15 @@ NSString * const MOAlreadyProtectedKey = @"moAlreadyProtectedKey";
     return [self objectForJSValue:jsValue];
 }
 
+- (id)evalString:(NSString *)string atURL:(NSURL *)url {
+    NSString* name = url ? [[url lastPathComponent] stringByDeletingPathExtension] : @"Untitled Script";
+    JSStringRef jsName = JSStringCreateWithUTF8CString([name UTF8String]);
+    JSGlobalContextSetName(_ctx, jsName);
+    JSStringRelease(jsName);
+    JSValueRef jsValue = [self evalJSString:string scriptPath:[url path]];
+    return [self objectForJSValue:jsValue];
+}
+
 - (JSValueRef)evalJSString:(NSString *)string {
     return [self evalJSString:string scriptPath:nil];
 }
