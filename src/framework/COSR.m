f05a8ced174ce07afd1a2809ba501c62b46dd4bf
August Mueller <gus@flyingmeat.com>
2014-06-24 21:15:16 -0700

    More interesting code that makes sense for nobody but me.

--- src/framework/COSR.m
+++ src/framework/COSR.m
@@ -7,10 +7,18 @@
 //
 
 #import "COSR.h"
+#import "COSDatabaseQueue.h"
+#import "COSDatabaseAdditions.h"
+#import "COSExtras.h"
+
+#define STRINGIZE(x) #x
+#define STRINGIZE2(x) STRINGIZE(x)
+#define SQLCode(text) @ STRINGIZE2(text)
 
 @interface COSR ()
 @property (strong) NSMutableDictionary *table;
 @property (strong) NSString *tablePath;
+@property (strong) COSDatabaseQueue *q;
 @end
 
 @implementation COSR
@@ -23,21 +31,57 @@
     return self;
 }
 
-+ (instancetype)tableWithPath:(NSString*)path {
+- (instancetype)tableWithPath:(NSString*)path {
     
     COSR *me = [COSR new];
     [me setTablePath:path];
     
+    [me setQ:[self q]];
+    
     return me;
+}
+
+
++ (instancetype)rootWithURL:(NSURL*)fileURL {
+    
+    COSR *me = [COSR new];
+    
+    [me setQ:[COSDatabaseQueue databaseQueueWithPath:[fileURL path]]];
+    [me setupDatabase];
+    
+    return me;
+}
+
+- (void)setupDatabase {
+    
+    [_q inDatabase:^(COSDatabase *db) {
+       
+        if (![db tableExists:@"R"]) {
+            
+            NSString *const rDB = SQLCode (
+             create table R (uniqueID text,
+                             name text not null,
+                             data blob,
+                             uti text not null,
+                             parentID text);
+             );
+            
+            if (![db executeUpdate:rDB]) {
+                NSLog(@"error creating R table: %@", [db lastError]);
+            }
+        }
+        
+    }];
+    
 }
 
 - (instancetype)subTableWithName:(NSString*)name {
     
     if (_tablePath) {
-        return [COSR tableWithPath:[_tablePath stringByAppendingFormat:@".%@", name]];
+        return [self tableWithPath:[_tablePath stringByAppendingFormat:@".%@", name]];
     }
     
-    return [COSR tableWithPath:name];
+    return [self tableWithPath:name];
 }
 
 - (instancetype)makeTable:(NSString*)tableName {
@@ -51,20 +95,74 @@
     return sub;
 }
 
+
 - (id)objectForKeyedSubscript:(NSString *)key {
-    debug(@"%s:%d", __FUNCTION__, __LINE__);
-    debug(@"key: '%@'", key);
+    
+    /*
+    This code is commented out, just because I wanted to see if the basics could be done.  It can.  It needs to be fancier for the future though.
+    __block id value = nil;
+    
+    [_q inDatabase:^(COSDatabase *db) {
+        NSString *query = @"select data, uti from R where name = ? and parentID = ?";
+     
+        COSResultSet *rs = [db executeQuery:query, key, _tablePath];
+        
+        if ([rs next]) {
+            
+            CFStringRef uti = (__bridge CFStringRef)[rs stringForColumn:@"uti"];
+            
+            if (UTTypeConformsTo(uti, kUTTypeText)) {
+                value = [rs stringForColumn:@"data"];
+            }
+            else {
+                value = [rs dataForColumn:@"data"];
+            }
+            
+            [rs close];
+        }
+        else {
+            NSLog(@"Could not find value for %@ in table %@", key, _tablePath);
+        }
+        
+    }];
+    
+    
+    if (value) {
+        return value;
+    }
+    */
     
     return [_table objectForKey:key];
 }
 
 - (void)setObject:(id)obj forKeyedSubscript:(NSString *)key {
-    debug(@"%s:%d", __FUNCTION__, __LINE__);
-    debug(@"obj: '%@'", obj);
-    debug(@"key: '%@'", key);
+    
+    /*
+    
+    [_q inDatabase:^(COSDatabase *db) {
+        NSString *sql = @"insert into R (uniqueID, name, data, uti, parentID) values (?, ?, ?, ?, ?)";
+        
+        
+        NSString *uuid = [NSString stringWithUUID];
+        NSString *uti = (id)kUTTypeData;
+        
+        if ([obj isKindOfClass:[NSString class]]) {
+            uti = (id)kUTTypeUTF8PlainText;
+        }
+        
+        if (![db executeUpdate:sql, uuid, key, obj, uti, _tablePath]) {
+            NSLog(@"Could not set value '%@' for '%@' in table '%@'", obj, key, _tablePath);
+        }
+        
+    }];
+    */
+    
+    if (!obj) {
+        [_table removeObjectForKey:key];
+        return;
+    }
     
     [_table setObject:obj forKey:key];
-    
 }
 
 @end
