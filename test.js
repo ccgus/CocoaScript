31b4e73e853062df0080cbd8e159efd511a142a7
Sam Deane <sam@elegantchaos.com>
2015-02-11 16:24:22 +0000

    Added a test script that exhibits the crash

---
+++ test.js
@@ -0,0 +1,11 @@
+framework("AppKit");
+framework("CoreGraphics");
+
+for (var n = 0; n < 20000; n++) {
+    print(n);
+    var path=NSBezierPath.bezierPath();
+    path.moveToPoint(NSMakePoint(0,0));
+    path.lineToPoint(NSMakePoint(10,10));
+    path.lineToPoint(NSMakePoint(150,200));
+    path.closePath();
+}
