a673e304eb5b8edf9b28f19861f35545978220fa
Sam Deane <sam@elegantchaos.com>
2015-12-17 09:57:06 +0000

    Use strong map again instead of dictionary. We will "leak" any object in the map until the JS reference is garbage collected, or until the end of the script session - which is not ideal, but better than crashing.

--- src/framework/mocha/Objects/MOBoxManager.m
+++ src/framework/mocha/Objects/MOBoxManager.m
@@ -11,13 +11,15 @@
 
 @implementation MOBoxManager {
     JSGlobalContextRef _context;
-    NSMutableDictionary *_index;
+    NSMapTable *_index;
 }
 
 - (instancetype)initWithContext:(JSGlobalContextRef)context {
     self = [super init];
     if (self) {
-        _index = [NSMutableDictionary new];
+        _index = [NSMapTable
+                           mapTableWithKeyOptions:NSMapTableStrongMemory | NSMapTableObjectPointerPersonality
+                           valueOptions:NSMapTableStrongMemory | NSMapTableObjectPointerPersonality];
         _context = context;
     }
 
@@ -26,24 +28,18 @@
 
 - (void)cleanup {
     NSAssert([NSThread isMainThread], @"should be main thread");
-    [_index enumerateKeysAndObjectsUsingBlock:^(id key, MOBox *box, BOOL *stop) {
-        NSLog(@"cleaned up box %@ for %@", box, [box.representedObject class]);
-//        JSValueUnprotect(_context, box.JSObject);
+    for (NSValue* key in _index) {
+        MOBox* box = [_index objectForKey:key];
+//        NSLog(@"cleaned up box %@ for %@", box, [box.representedObject class]);
         [box disassociateObject];
-    }];
+    }
     _index = nil;
     _context = nil;
 }
 
-- (id)keyForObject:(id)object {
-    NSValue *objectPointerValue = [NSValue valueWithPointer:(__bridge const void *)(object)];
-    return objectPointerValue;
-}
-
 - (MOBox*)boxForObject:(id)object {
     NSAssert([NSThread isMainThread], @"should be main thread");
-    id key = [self keyForObject:object];
-    MOBox* box = [_index objectForKey:key];
+    MOBox* box = [_index objectForKey:object];
     return box;
 }
 
@@ -52,18 +48,17 @@
     MOBox* box = [[MOBox alloc] initWithManager:self];
     JSObjectRef jsObject = JSObjectMake(_context, jsClass, (__bridge void *)(box));
     [box associateObject:object jsObject:jsObject];
-//    JSValueProtect(_context, jsObject); // TODO: this is a temporary hack. It will fix the script crash, but only at the expense of leaking all JS objects during a script run. Which is not good...
-    [_index setObject:box forKey:[self keyForObject:object]];
+    NSAssert([_index objectForKey:object] == nil, @"shouldn't already have an entry for the object");
+    [_index setObject:box forKey:object];
     return jsObject;
 }
 
 - (void)removeBoxForObject:(id)object {
     NSAssert([NSThread isMainThread], @"should be main thread");
-    id key = [self keyForObject:object];
-    MOBox* box = [_index objectForKey:key];
+    MOBox* box = [_index objectForKey:object];
     if (box) {
         [box disassociateObject];
-        [_index removeObjectForKey:key];
+        [_index removeObjectForKey:object];
     }
 }
 
